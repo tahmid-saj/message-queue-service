f5909aaf1496f993f374e48ba3383f19eb24fa97
tahmid-saj <tahmid.saj@gmail.com>
2024-11-07 15:41:03 -0500

    testing queue and message ops

--- main.go
+++ main.go
@@ -1,1 +1,54 @@
 package main
+
+import (
+
+)
+
+func main() {
+	// CreateQueue
+	// res, err := queue.CreateQueue("test-queue")
+	// if err != nil {
+	// 	return
+	// }
+	// fmt.Println(res)
+
+	// ListQueues
+	// res, err := queue.ListQueues()
+	// if err != nil {
+	// 	return
+	// }
+	// fmt.Println(res)
+
+	// GetQueueURL
+	// res, err := queue.GetQueueURL("test-queue")
+	// if err != nil {
+	// 	return
+	// }
+	// fmt.Println(res)
+
+	// SendMessage
+	// message := queue.Message{
+	// 	Subject: "test-subject",
+	// 	Body: "test-body",
+	// 	Timestamp: time.Now(),
+	// }
+	// res, err := queue.SendMessage("test-queue", message)
+	// if err != nil {
+	// 	return
+	// }
+	// fmt.Println(res)
+
+	// ReceiveMessage
+	// res, err := queue.ReceiveMessage("test-queue", 10)
+	// if err != nil {
+	// 	return
+	// }
+	// fmt.Println(res)
+
+	// DeleteMessage
+	// res, err := queue.DeleteMessage("test-queue", "AQEBZfSu31MNOh0xF11I6EhSdvvk7GXBMQnryIo85jD7kRYlv8vkrqeSjcsGaAZtRkqLs3thfR0cfpufJjMfhfepCJJDvBfeWQ7T6UX2Kazh5ipvWfwcQwCj9hp4D8Qe8kmfvstG6S03mvez2vSKYJ6BBZzYU5j5EGJsiEQj0VHIDzUbKyWKcIuOiAtpBFf32ZyIJC9+KnHRgBLaZq4ZJATZo02/w//HNhMB4yo8dOrArtz4Li3QDw/JFhab1q4OTQBh3brWwl4PjnxtJNSE7nZsZxtTj0qFriOcZgrGtQeQkvURkItCEaU/xZhMbNOi3bL7JVP6tVDO0UlwsTyg5QqyoV5/Fbhy14P/n6icC0Dq0pXXk8TZM+38aE/FeQrp6OqOQxFWPfInP7wQsiVYSM5E7Q==")
+	// if err != nil {
+	// 	return
+	// }
+	// fmt.Println(res)
+}
