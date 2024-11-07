95b4e5dc2c1fa4adce7eab2b9406a875aa6b072b
tahmid-saj <tahmid.saj@gmail.com>
2024-11-07 15:21:01 -0500

    message ops

--- queue/message.go
+++ queue/message.go
@@ -1,6 +1,7 @@
 package queue
 
 import (
+	"fmt"
 	"log"
 	"time"
 
@@ -47,6 +48,77 @@ func SendMessage(queueName string, message Message) (bool, error) {
 		},
 		MessageBody: aws.String(message.Body),
 		QueueUrl: queueUrl,
+	})
+	if err != nil {
+		log.Println(err)
+		return false, err
+	}
+
+	return true, nil
+}
+
+func ReceiveMessage(queueName string, visibilityTimeout int) (*sqs.Message, error) {
+	if visibilityTimeout < 0 { visibilityTimeout = 0 }
+	if visibilityTimeout > 12 * 60 * 60 { visibilityTimeout = 12 * 60 * 60 }
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := sqs.New(sess)
+
+	result, err := svc.GetQueueUrl(&sqs.GetQueueUrlInput{
+		QueueName: &queueName,
+	})
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	queueUrl := result.QueueUrl
+
+	messageResult, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
+		AttributeNames: []*string{
+			aws.String(sqs.MessageSystemAttributeNameSentTimestamp),
+		},
+		MessageAttributeNames: []*string{
+			aws.String(sqs.QueueAttributeNameAll),
+		},
+		QueueUrl: queueUrl,
+		MaxNumberOfMessages: aws.Int64(1),
+		VisibilityTimeout: aws.Int64(int64(visibilityTimeout)),
+	})
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	message := *messageResult.Messages[0]
+	fmt.Println("Message: " + *message.ReceiptHandle)
+	
+	return &message, nil
+}
+
+func DeleteMessage(queueName, receiptHandleMessage string) (bool, error) {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := sqs.New(sess)
+
+	result, err := svc.GetQueueUrl(&sqs.GetQueueUrlInput{
+		QueueName: &queueName,
+	})
+	if err != nil {
+		log.Println(err)
+		return false, err
+	}
+
+	queueUrl := result.QueueUrl
+
+	_, err = svc.DeleteMessage(&sqs.DeleteMessageInput{
+		QueueUrl: queueUrl,
+		ReceiptHandle: &receiptHandleMessage,
 	})
 	if err != nil {
 		log.Println(err)
